2e8763a1c63bf7b6838bc41a55459b032d25380b
Mohammad-Momeni <momeni00022@gmail.com>
2023-10-30 18:09:19 +0330

    getActions function

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
-def isBlocked(boardArray, x, y):
-    if boardArray[x][y] == 'w':
-        return False
-    if boardArray[x][y] == 'b':
-        pass
-def get_actions(boardArray, start):
+def isBlocked(boardArray, start, direction):
+    x, y = start[0], start[1]
+    if direction == 'u':
+        if boardArray[x - 1][y] == 'w':
+            return True
+        if boardArray[x - 1][y] == 'b' and (x - 2 < 0 or boardArray[x - 2][y] == 'b'):
+            return True
+        if boardArray[x - 1][y] != 'b' or (x - 2 >= 0 and boardArray[x - 2][y] != 'b'):
+            return False
+    elif direction == 'd':
+        if boardArray[x + 1][y] == 'w':
+            return True
+        if boardArray[x + 1][y] == 'b' and (x + 2 >= len(boardArray) or boardArray[x + 2][y] == 'b'):
+            return True
+        if boardArray[x + 1][y] != 'b' or (x + 2 < len(boardArray) and boardArray[x + 2][y] != 'b'):
+            return False
+    elif direction == 'l':
+        if boardArray[x][y - 1] == 'w':
+            return True
+        if boardArray[x][y - 1] == 'b' and (y - 2 < 0 or boardArray[x][y - 1] == 'b'):
+            return True
+        if boardArray[x][y - 1] != 'b' or (y - 2 >= 0 and boardArray[x][y - 1] != 'b'):
+            return False
+    elif direction == 'r':
+        if boardArray[x][y + 1] == 'w':
+            return True
+        if boardArray[x][y + 1] == 'b' and (y + 2 >= len(boardArray[x]) or boardArray[x][y + 2] == 'b'):
+            return True
+        if boardArray[x][y + 1] != 'b' or (y + 2 < len(boardArray[x]) and boardArray[x][y + 2] != 'b'):
+            return False
+
+def getActions(boardArray, start):
         actions = []
         x, y = start[0], start[1]
-        if(x - 1 >= 0 and (not boardArray[x - 1][y].isBlocked)):
-            actions.append()
-        if(x + 1 < params.rows and not boardArray[x + 1][y].isBlocked):
-            actions.append({'x': 1, 'y': 0})
-        if(y - 1 >= 0 and not boardArray[x][y - 1].isBlocked):
-            actions.append({'x': 0, 'y': -1})
-        if(y + 1 < params.cols and not boardArray[x][y + 1].isBlocked):
-            actions.append({'x': 0, 'y': 1})
+        if(x - 1 >= 0 and not isBlocked(boardArray, start, 'u')):
+            actions.append('u')
+        if(x + 1 < len(boardArray) and not isBlocked(boardArray, start, 'd')):
+            actions.append('d')
+        if(y - 1 >= 0 and not isBlocked(boardArray, start, 'l')):
+            actions.append('l')
+        if(y + 1 < len(boardArray[x]) and not isBlocked(boardArray, start, 'r')):
+            actions.append('r')
         return actions
+
 def findStart(boardArray):
     for i in range(len(boardArray)):
         if 'a' in boardArray[i]:
@@ -33,7 +60,7 @@ def bfs_find(boardArray):
         new_path = open_list.pop(0)
         start_point = new_path[len(new_path) - 1]
         x, y = start_point[0], start_point[1]
-        actions = get_actions(self.current_state, start_point)
+        actions = getActions(self.current_state, start_point)
         new_start = x - 1, y
         if {'x':-1, 'y': 0} in actions and not new_start in  new_path:
             open_list.append(new_path + [new_start])
@@ -49,7 +76,7 @@ def bfs_find(boardArray):
     return generalVisited, new_path
 
 def bfs(boardArray):
-    
+    start = findStart(boardArray)
     visited, path = bfs_find(boardArray)
     return path
 
@@ -58,5 +85,5 @@ if __name__ == '__main__':
                   ['a', 'b', 'f'],
                   ['f', 'f', 'f'],
                 ]
-    print(findStart(boardArray=boardArray))
-    print(boardArray[0])
+    start = findStart(boardArray)
+    print(getActions(boardArray, start))
